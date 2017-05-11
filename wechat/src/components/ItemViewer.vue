82d6d1bcdd9154906bf8c3d2f50d179cfe5d8cdd
fordream001 <littlepigwang@outlook.com>
2017-05-11 14:47:54 +0800

    Add: category and viewer components

---
+++ wechat/src/components/ItemViewer.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+  <section class="viewer-box">
+    <div class="title-box">
+      <span class="title">
+        新书速递
+      </span>
+      <span class="view-changer" @click="changeView">
+        {{ isList ? '网格视图' : '列表视图' }}
+      </span>
+    </div>
+    <div class="viewer-block">
+      <div v-for="item in currentItems" class="viewer-item">
+        <img src="../images/s3083552.jpg" height="128" width="88">
+        <span>资治通鉴</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    items: Array
+  },
+  data () {
+    return {
+      isList: false
+    }
+  },
+  computed: {
+    currentItems () {
+      return this.items
+    }
+  },
+  methods: {
+    changeView () {
+      this.isList = !this.isList
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.viewer-block {
+  display: flex;
+}
+.viewer-item {
+  width: 33%;
+}
+</style>
--- wechat/src/pages/home/children/indexpage.vue
+++ wechat/src/pages/home/children/indexpage.vue
@@ -1,22 +1,86 @@
 <template lang="html">
-  <main class="main-part">
-    <swiper :list="imgList"></swiper>
-  </main>
+  <section class="main-part">
+    <swiper :list="imgList" v-model="imgIndex"></swiper>
+    <div class="category">
+      <div class="cate-item">马列主义</div>
+      <div class="cate-item">哲学宗教</div>
+      <div class="cate-item">自然科学</div>
+      <div class="cate-item">数理科学</div>
+      <div class="cate-item">社会科学</div>
+      <div class="cate-item">政治法律</div>
+      <div class="cate-item">工业技术</div>
+      <div class="cate-item">更多分类</div>
+       <!--TODO
+       Add the more cate page
+       -->
+    </div>
+    <item-viewer :items="books"></item-viewer>
+  </section>
 </template>
 
 <script>
 import { Swiper } from 'vux'
+import ItemViewer from '@/components/ItemViewer'
+
 export default {
   components: {
-    Swiper
+    Swiper,
+    ItemViewer
   },
   data () {
     return {
-      imgList: ['../../assets/logo.png', '../../assets/logo.png', '../../assets/logo.png']
+      imgList: [{
+        url: 'javascript:',
+        img: 'https://static.vux.li/demo/1.jpg',
+        title: '送你一朵fua'
+      }, {
+        url: 'javascript:',
+        img: 'https://static.vux.li/demo/2.jpg',
+        title: '送你一辆车'
+      }, {
+        url: 'javascript:',
+        img: 'https://static.vux.li/demo/3.jpg',
+        title: '送你一次旅行'
+      }],
+      imgIndex: 0,
+      books: [
+        {
+          img: '',
+          title: '资治通鉴',
+          isbn: '123456'
+        },
+        {
+          img: '',
+          title: '资治通鉴',
+          isbn: '123456'
+        },
+        {
+          img: '',
+          title: '资治通鉴',
+          isbn: '123456'
+        },
+        {
+          img: '',
+          title: '资治通鉴',
+          isbn: '123456'
+        }
+      ]
     }
   }
 }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
+.category {
+  display: flex;
+  flex-flow: row wrap;
+  background-color: #fff;
+  padding: 15px 0;;
+}
+.cate-item {
+  text-align: center;
+  color: #5f5f5f;
+  font-size: 12px;
+  width: 25%;
+}
 </style>
