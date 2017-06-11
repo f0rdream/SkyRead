f1edaf33018165457358ec9223a0386b85a45955
唐宗钰 <867581452@qq.com>
2017-06-11 11:01:42 +0800

    Admin Holoding

--- backend/Backendapi/bookdata/admin.py
+++ backend/Backendapi/bookdata/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book,Refer
+from .models import Book,Refer,Holiding
 admin.site.register(Book)
 admin.site.register(Refer)
+admin.site.register(Holiding)
 # Register your models here.
