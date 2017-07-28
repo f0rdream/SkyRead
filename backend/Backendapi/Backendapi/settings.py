b3dadd33ba1629e20548739d9e73c506bc914b29
SkyRead <867581452@qq.com>
2017-07-28 15:06:04 +0800

    debug

--- backend/Backendapi/Backendapi/settings.py
+++ backend/Backendapi/Backendapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')-+8!#aw1ad%z!*ilh3bia=za&3j$n(cdqtudmg*uc$!w_+5^9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
