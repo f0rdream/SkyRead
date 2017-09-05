dfadb92c5a10daaaf80666591bd62c9c812ea4ea
SkyRead <867581452@qq.com>
2017-09-05 09:21:08 +0800

    change author

--- backend/Backendapi/bookdata/serializers.py
+++ backend/Backendapi/bookdata/serializers.py
@@ -85,6 +85,7 @@ class BookInfoSerializer(ModelSerializer):
             for author in authors:
                 if author == '' or author == '[美国]' or author == '[美]' \
                         or author == '[日]' or author == ['奥地利'] or author == ['瑞典']:
+                    continue
                 else:
                     if len(author) > 15:
                         author = author[:15]
