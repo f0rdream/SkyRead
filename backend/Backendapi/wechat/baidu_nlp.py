b0eef63a7246e8b9967247fa8c753a429761b0d4
SkyRead <867581452@qq.com>
2017-09-05 23:46:05 +0800

    Fix:

--- backend/Backendapi/wechat/baidu_nlp.py
+++ backend/Backendapi/wechat/baidu_nlp.py
@@ -5,14 +5,7 @@
 @Author  : TangZongYu
 @Desc    : 
 """
-from aip import AipNlp
-# 定义常量
-APP_ID = '10091919'
-API_KEY = 'S4SVTBvaB4zIWaAnU9W2u9CQ'
-SECRET_KEY = 'bFFrfPgq69Hv6fyD75bvFBAr0RODMkdM'
-
-# 初始化AipNlp对象
-aipNlp = AipNlp(APP_ID, API_KEY, SECRET_KEY)
-result = aipNlp.wordpos("想学习机器学习")
-
-print result
+import jieba.posseg as pseg
+words = pseg.cut("想学习机器学习")
+for word in words:
+    print word,word.flag
---
+++ backend/Backendapi/wechat/nlp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 17-9-5 下午11:42
+@Author  : TangZongYu
+@Desc    : 
+"""
+
+def get_seach(content):
+    import jieba.posseg as pseg
+    words = pseg.cut(content)
+    for word in words:
+        print word, word.flag
